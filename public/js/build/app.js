713f2961111638b83844f3b20158f99e661bdd8d
Andrew Varnerin <andrew@varnerin.info>
2015-07-03 01:04:12 -0400

    show ride details

--- public/js/build/app.js
+++ public/js/build/app.js
@@ -1,8 +1,28 @@
 (function() {
-  var app = angular.module('hapi-strava', ['restangular']);
+  var app = angular.module('hapi-strava', ['restangular', 'ui.router']);
 
-  app.config(['RestangularProvider', function(rest) {
+  app.config(['RestangularProvider', '$stateProvider', '$urlRouterProvider', function(rest, states, urlRouter) {
     rest.setBaseUrl('/api/v1');
+
+    urlRouter.otherwise('/activities');
+
+    states
+      .state('activities', {
+        url: '/activities',
+        templateUrl: '/views/activities.tpl.html',
+        controller: 'ActivitiesCtrl as activitiesCtrl'
+      }).state('activities.details', {
+        url: '/{id:int}',
+        templateUrl: '/views/activity.tpl.html',
+        resolve: {
+          activity: function(ActivitiesSvc, $stateParams) {
+            return ActivitiesSvc.get($stateParams.id).then(function(data) {
+              return data;
+            });
+          }
+        },
+        controller: 'ActivityCtrl as activityCtrl'
+      });
   }]);
 
   ActivitiesSvc.$inject = ['Restangular'];
@@ -15,11 +35,25 @@
     return this.allActivities.getList().$object;
   }
 
+  ActivitiesSvc.prototype.get = function(id) {
+    return this.allActivities.getList().then(function(data) {
+      return _.find(data, function(activity) {
+        return activity.id === id;
+      });
+    });
+  }
+
   ActivitiesCtrl.$inject = ['ActivitiesSvc'];
   function ActivitiesCtrl(activitiesSvc) {
     this.activities = activitiesSvc.list();
   }
 
+  ActivityCtrl.$inject = ['ActivitiesSvc', 'activity'];
+  function ActivityCtrl(activitiesSvc, activity) {
+    this.activity = activity;
+  }
+
   app.service('ActivitiesSvc', ActivitiesSvc);
   app.controller('ActivitiesCtrl', ActivitiesCtrl);
+  app.controller('ActivityCtrl', ActivityCtrl);
 })();
